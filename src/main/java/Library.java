e3700c093539fb7cf61f91e81d453c7c0ede4585
tannyque <tannyque1992@gmail.com>
2018-09-25 16:27:30 +0100

    Completes MVP - Adds book to library :books;

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,12 +3,20 @@ import java.util.ArrayList;
 public class Library {
 
     private ArrayList<Book> books;
+    private int capacity;
 
-    public Library() {
+    public Library(int capacity) {
         this.books = new ArrayList<>();
+        this.capacity = capacity;
     }
 
     public int bookCount() {
         return books.size();
+    }
+
+    public void addBookToLibrary(Book book) {
+        if (bookCount() < capacity) {
+            this.books.add(book);
+        }
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -6,14 +6,31 @@ import static org.junit.Assert.assertEquals;
 public class LibraryTest {
 
     Library library;
+    Book book;
 
     @Before
     public void setUp() {
-        library = new Library();
+        library = new Library(3);
     }
 
     @Test
     public void libraryStartsEmpty() {
         assertEquals(0, library.bookCount());
+    }
+
+    @Test
+    public void canAddBookToLibrary() {
+        library.addBookToLibrary(book);
+        assertEquals(1, library.bookCount());
+    }
+
+    @Test
+    public void cannotAddBookToLibrary() {
+        library.addBookToLibrary(book);
+        library.addBookToLibrary(book);
+        library.addBookToLibrary(book);
+        assertEquals(3, library.bookCount());
+        library.addBookToLibrary(book);
+        assertEquals(3, library.bookCount());
     }
 }
